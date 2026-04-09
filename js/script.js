1c0f84109b9cdb3bd43d9ef5dd7b8f0ce86cfa6c
brunafernxndes <brunafernandess@outlook.com.br>
2026-04-09 16:32:31 -0300

    Commit Inicial

---
+++ js/script.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        }
+    }
+}
+
+// Ativar o primeiro reveal se já estiver na tela
+reveal();
